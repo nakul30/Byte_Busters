ccf142630b233b6046553637aa15dbc62d809fcd
Nakul <nakulgarg1424@gmail.com>
2023-10-15 16:32:30 +0530

    1.3

--- stgi_doc_generator/controllers/converter_controller.js
+++ stgi_doc_generator/controllers/converter_controller.js
@@ -1,8 +1,20 @@
 const User = require('../models/user');
-const Document = require('../models/file');
+const Document = require('../models/hfile');
+// const User = require('path-to-user-model'); // Replace with the actual path to your User model
+
 module.exports.loadpage = function (req, res) {
-    return res.render('convertpage', {
-        title: "Converter",
-        // console.log(req.user.name) 
-    })
-}; 
+    User.findById(req.user.id)
+        .populate({ path: 'products' })
+        .exec()
+        .then((user) => {
+            res.render('converterpage', {
+                title: "Converter",
+                all_users: user
+            });
+        })
+        .catch((err) => {
+            console.log("ERROR: ", err);
+            // Handle the error or render an error page
+        });
+};
+ 
